283ce611e3256ce3319800e87441f8f55487fb3c
kelvinmuchui <muchuikelvin423@gmail.com>
2016-06-14 19:35:07 +0300

    add css

--- app.py
+++ app.py
@@ -36,26 +36,27 @@ def welcome():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-        	session['logged_in']= True
-        flash('You have logged in ')
-        return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+  if request.method == 'POST':
+      if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+          error = 'Invalid Credentials. Please try again.'
+      else:
+      	session['logged_in']= True
+      flash('You have logged in ')
+      return redirect(url_for('home'))
+  return render_template('login.html', error=error)
 
 
 @app.route('/logout')
 @login_required
 def logout():
-	session.pop('logged_in', None)
+session.pop('logged_in', None)
 
-        flash('You have logged out')
-	return redirect(url_for('welcome'))
+      flash('You have logged out')
+return redirect(url_for('welcome'))
 
 
 def connect_db():
-    return sqlite3.connect(app.database)
+  return sqlite3.connect(app.database)
+  
 if __name__ == '__main__':
     app.run(debug = True) 
